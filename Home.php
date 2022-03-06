dd38747feda69b8efe8c75fab3f02e111e2e48c8
Harry2224 <99489544+Harry2224@users.noreply.github.com>
2022-03-06 17:16:50 -0500

    HARJINDER KAUR (2111536)  06-03-2022  COMPLETE THE FILE REQUIREMENTS

--- Home.php
+++ Home.php
@@ -1,76 +1,70 @@
 
 
 <?php
-define("FILE_INDEX", "index.php");
-define("FILE_HOME", "Home.php");
-define("FILE_BUYING", "Buying.php");
-define("FILE_ORDERS", "Orders.php");
-
-define ("FOLDER_PICTURES", "pictures/");
-//define("PICTURE_PEPSI", FOLDER_PICTURES . "pepsi.jpg");
-//define("PICTURE_COCA_COLA", FOLDER_PICTURES . "coca.jpg");
-//define("PICTURE_BEAR", FOLDER_PICTURES . "beer.jpg");
-define ("FILE_LOGO",FOLDER_PICTURES. "plant.png");
-define ("FOLDER_CSS","CSS/");
-define ("FILE_CSS", FOLDER_CSS . "style.css");
-
-//define ("FILE_CSS_BIG", FOLDER_CSS . "contact.php");
-
-
-
-
-function pageTop($pageTitle)
-{
-?><html>
-    <head><meta charset ="UTF-8">
-       
-        <title><?php echo $pageTitle ?></title>
-        <style type="text/css">
- body{ background-color:purple; }
- table{ background-color:aqua; }
- h1{ color:green; }
- #buttonAlign {	padding-left:25px; }
-</style>
-    </head>
-    <body>
-        <table width="100%">
-            <tr>
-                
-                <td> <img src="<?php echo FILE_LOGO; ?>"
-             class="logo"
-             alt="company logo" height="200" width="600"> </td>
-                <td align="center"> <h1 style="font-family:Lucida Handwriting;">Indoor Plants</h1> 
-                    <p style="font-family:Lucida Handwriting;">Indoor plants improve air quality in other ways as well.
-                    Plants release water vapor into the air, which increases humidity,                            
-      and this can help improve respiratory and skin health by offsetting the drying effects of heating systems.</p></td></tr>
-             <?php
-navigationMenu();
-
-}
-   function navigationMenu()
-   { ?>
-    
-       <tr><td> <a href="<?php echo FILE_INDEX;?>">Homepage</a></td></tr>
-       <tr><td><a href="<?php echo FILE_BUYING;?>">Buying</a></td></tr>
-       <tr><td><a href="<?php echo FILE_ORDERS;?>">Orders</a></td></tr> 
-   
-    <?php
-     
-}
-function pageBottom()
-{
+include_once 'PHPfunctions.php';
+define("PICTURE_A1", FOLDER_PICTURES . "a1.jpg");
+define("PICTURE_A2", FOLDER_PICTURES . "a2.jpg");
+define("PICTURE_A3", FOLDER_PICTURES . "a3.jpg");
+define("PICTURE_A4", FOLDER_PICTURES . "a4.jpg");
+define("PICTURE_B4", FOLDER_PICTURES . "b4.jpeg"); {
     ?>
-        <?php
-$myArray = array(PICTURE_A1, PICTURE_A2, PICTURE_A3, PICTURE_A4, PICTURE_B4 );
- shuffle($myArray);
- ?> 
+     <!DOCTYPE html>
+     <html>
+        <head><meta charset ="UTF-8">
 
-                    <img src="<?php echo $myArray[0]; ?>" class ="advertising" alt="advertising" height ="400" width="650">
-    
-        
-         
-        </body>
-        </html>
-  <?php
+            <title><?php echo $pageTitle ?></title>
+            <style type="text/css">
+                body{
+                    background-color:purple;
+                }
+                table{
+                    background-color:highlighttext;
+                }
+                h1{
+                    color:green;
+                }
+                #buttonAlign {
+                    padding-left:25px;
+                }
+            </style>
+        </head>
+
+        <body>
+            <table width="100%">
+                <tr>
+
+                    <td> <img src="<?php echo FILE_LOGO; ?>"
+                              class="logo"
+                              alt="company logo" height="200" width="600"> </td>
+                    <td align="center"><h1 style="font-family:Lucida Handwriting;">Indoor Plants</h1> 
+                        <p style="font-family:Lucida Handwriting;">Indoor plants improve air quality in other ways as well.
+                            Plants release water vapor into the air, which increases humidity,                            
+                            and this can help improve respiratory and skin health by offsetting the drying effects of heating systems.</p></td></tr>
+
+
+                <tr><td> <a href="<?php echo FILE_INDEX; ?>">Homepage</a></td></tr>
+                <tr><td><a href="<?php echo FILE_BUYING; ?>">Buying</a></td></tr>
+                <tr><td><a href="<?php echo FILE_ORDERS; ?>">Orders</a></td></tr>
+    <?php
+    $myArray = array(PICTURE_A1, PICTURE_A2, PICTURE_A3, PICTURE_A4, PICTURE_B4);
+    shuffle($myArray);
+    ?> 
+
+                      <td>
+                    <a href="https://foli.ca/collections/small-plant-pot-combo">
+                        <img src="<?php echo $myArray[0]; ?>" class ="advertising" alt="advertising" height ="400" width="650"><\a?>
+                        </td>
+
+                        </table>
+
+
+                        </body>
+                        <footer class="footer">
+                            <div class="footerContainer">
+                                  <p class="copyright">COPYRIGHT HARJINDER KAUR 2111536 (<?php echo date("Y"); ?>)</p>
+                            </div>
+                        </footer>
+                        </html>
+    <?php
 }
 ?>
